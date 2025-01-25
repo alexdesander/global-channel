cdabc33050f2a40b51e411da82e05d6507afc828
alexdesander <alexander.heim@student.uibk.ac.at>
2025-01-25 21:02:48 +0100

    Fixed export errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,44 @@
-/// Creates a new global/static channel with the given name, bounds and item type.
+pub extern crate crossbeam_channel;
+pub use paste;
+pub use sync_unsafe_cell;
+
+/// Creates a new global/static channel with the given name, bounds, and item type.
 ///
-/// Usage example: global_channel!(name, None, u32); // Unbounded channel of u32
+/// # Example
+///
+/// ```rust
+/// use global_channel::global_channel;
+///
+/// global_channel!(my_channel, None, u32);
+///
+/// fn main() {
+///     let tx = my_channel_tx();
+///     let rx = my_channel_rx();
+///     tx.send(42).unwrap();
+///     assert_eq!(rx.recv().unwrap(), 42);
+/// }
 #[macro_export]
 macro_rules! global_channel {
     ($channel_name:ident, $bounds:expr, $item:ty) => {
-        paste::paste! {
+        $crate::paste::paste! {
             #[allow(non_upper_case_globals)]
-            static [<__ $channel_name _TX>]: ::sync_unsafe_cell::SyncUnsafeCell<
-                ::std::mem::MaybeUninit<::crossbeam_channel::Sender<$item>>
-            > = ::sync_unsafe_cell::SyncUnsafeCell::new(::std::mem::MaybeUninit::uninit());
+            static [<__ $channel_name _TX>]: $crate::sync_unsafe_cell::SyncUnsafeCell<
+                ::std::mem::MaybeUninit<$crate::crossbeam_channel::Sender<$item>>
+            > = $crate::sync_unsafe_cell::SyncUnsafeCell::new(::std::mem::MaybeUninit::uninit());
 
             #[allow(non_upper_case_globals)]
-            static [<__ $channel_name _RX>]: ::sync_unsafe_cell::SyncUnsafeCell<
-                ::std::mem::MaybeUninit<::crossbeam_channel::Receiver<$item>>
-            > = ::sync_unsafe_cell::SyncUnsafeCell::new(::std::mem::MaybeUninit::uninit());
+            static [<__ $channel_name _RX>]: $crate::sync_unsafe_cell::SyncUnsafeCell<
+                ::std::mem::MaybeUninit<$crate::crossbeam_channel::Receiver<$item>>
+            > = $crate::sync_unsafe_cell::SyncUnsafeCell::new(::std::mem::MaybeUninit::uninit());
 
-            // The setup function that initializes the channel exactly once
+            // Initializes the channel once per process
             #[allow(non_snake_case)]
             fn [<setup_channel_ $channel_name>](bounds: Option<usize>) {
                 static INIT: ::std::sync::Once = ::std::sync::Once::new();
                 INIT.call_once(|| {
                     let (tx, rx) = match bounds {
-                        Some(b) => ::crossbeam_channel::bounded(b),
-                        None => ::crossbeam_channel::unbounded(),
+                        Some(b) => $crate::crossbeam_channel::bounded(b),
+                        None => $crate::crossbeam_channel::unbounded(),
                     };
                     unsafe {
                         let tx_ref = &mut *[<__ $channel_name _TX>].get();
@@ -33,15 +49,16 @@ macro_rules! global_channel {
                 });
             }
 
-            // Public function to get the Sender
             #[allow(non_snake_case)]
-            pub fn [<$channel_name _tx>]() -> &'static ::crossbeam_channel::Sender<$item> {
+            #[inline]
+            pub fn [<$channel_name _tx>]() -> &'static $crate::crossbeam_channel::Sender<$item> {
                 [<setup_channel_ $channel_name>]($bounds);
                 unsafe { &*(*[<__ $channel_name _TX>].get()).as_ptr() }
             }
 
             #[allow(non_snake_case)]
-            pub fn [<$channel_name _rx>]() -> &'static ::crossbeam_channel::Receiver<$item> {
+            #[inline]
+            pub fn [<$channel_name _rx>]() -> &'static $crate::crossbeam_channel::Receiver<$item> {
                 [<setup_channel_ $channel_name>]($bounds);
                 unsafe { &*(*[<__ $channel_name _RX>].get()).as_ptr() }
             }
@@ -51,10 +68,12 @@ macro_rules! global_channel {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     global_channel!(test_channel, None, u32);
 
     #[test]
-    fn test_channel() {
+    fn test_basic_usage() {
         let tx = test_channel_tx();
         let rx = test_channel_rx();
         tx.send(42).unwrap();
